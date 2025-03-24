47c91b4d64f43eb29b1748d2f9f64f4f1de78ef0
gowthamkamparaju <87009905+gowthamkamparaju@users.noreply.github.com>
2025-03-24 11:47:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,17 @@ terraform {
       version = "~> 5.0"
 
     }
-   }
-  }
+
+  }
+
+}
 
 
 
 provider "aws" {
 
   region = "ap-south-1"
+
 }
 
 
@@ -28,12 +31,23 @@ resource "aws_instance" "commit_instances" {
   ami           = "ami-05c179eced2eb9b5b" # Replace with your AMI ID
 
   instance_type = "t2.micro"
-  tags = {
-   Name = "GitHub-Commit-Instance-${timestamp()}"
- }
+
+
+
+  tags = {
+
+    Name = "GitHub-Commit-Instance-${timestamp()}"
+
+  }
+
 }
 
+
+
 output "instance_public_ip" {
- value       = aws_instance.commit_instances[0].public_ip
- description = "Public IP address of the EC2 instance"
+
+  value       = aws_instance.commit_instances[0].public_ip
+
+  description = "Public IP address of the EC2 instance"
+
 }
