9c4c0c39384f2deeed6d8ee747593567b46e403c
gowthamkamparaju <87009905+gowthamkamparaju@users.noreply.github.com>
2025-08-11 15:30:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins + Docker on AWS!');
+  res.send('Hello Deploy application with Docker!');
 });
  
 app.listen(3000, () => {
