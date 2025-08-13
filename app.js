d6e7b1bbfe96d4b29e1691693775c157d01b5f6f
gowthamkamparaju <87009905+gowthamkamparaju@users.noreply.github.com>
2025-08-13 11:06:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('Hello Deploy application with Docker!');
+  res.send('Hello Deploy application with Docker');
 });
  
 app.listen(3000, () => {
