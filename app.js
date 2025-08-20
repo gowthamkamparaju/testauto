5b98290bf4ebe126651206b01b344d11e6887579
gowthamkamparaju <87009905+gowthamkamparaju@users.noreply.github.com>
2025-08-20 18:16:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send('Hello from GitHubActions + Docker on AWS');
+  res.send('Hello from GitHubActions + Docker on AWS aug 20');
 });
  
 app.listen(3000, () => {
